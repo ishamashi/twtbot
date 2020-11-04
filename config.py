3561e4932a4e13f58b12d8a937a4ead76acb9965
ichoprisma <icho@prisma-ads.com>
2020-11-04 15:47:12 +0700

    first commit

---
+++ config.py
@@ -0,0 +1,6 @@
+consumer_key = "qsNlsXToiQyGuGYortD4vBt6F"
+consumer_secret = "gN4Cqd931JdWzPgmqReW7Bu6OdVvHVKUqBcQElqi0cKHE3U58Z"
+access_token = "1144238737796157440-9wGKrQ1G7lmPvMVTJGvFmAUD8n1D65"
+access_token_secret = "oz5TKUdLn7yvC4Zt744dRiqqpDtegBHBKYCRbHEqSLE77"
+trigger = "[triger]"
+timezone = 7 #change this into your timezone. for example if your timezone is GMT+7 you put 7
